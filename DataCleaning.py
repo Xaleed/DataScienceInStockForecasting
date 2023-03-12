24c1f44f4a57b4b65699c47e001fbafbd4d1cd9a
Khaled Masoumifard <masoumifardk@yahoo.com>
2023-03-12 23:54:49 +0330

    The first run of the model has some error

--- DataCleaning.py
+++ DataCleaning.py
@@ -16,19 +16,14 @@ from pytse_client import download_client_types_records
 
 
 #Download historical data of trades
-tickers = tse.download(symbols="all", write_to_csv=True,  include_jdate=True)
+#tickers = tse.download(symbols="all", write_to_csv=True,  include_jdate=True)
 
 
 # In[3]:
-
-
-path = r'C:\\Users\\masoumifard.kh\\CodsOfprojects\\Public\\PredictionOfPumpAndDump\\tickers_data'
+#path = r'C:\\Users\\masoumifard.kh\\CodsOfprojects\\Public\\PredictionOfPumpAndDump\\tickers_data'
+path = r"/home/khaled/Project/DataScienceInStockForecasting/tickers_data/"
 filenames = glob.glob(path + "/*.csv")
-
-
 # In[4]:
-
-
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
@@ -40,12 +35,33 @@ for filename in filenames:
     dd.insert(1,"PersianNameOfCode", strs, True)
     dfs.append(dd)
 big_frame = pd.concat(dfs, ignore_index=True)
-
-big_frame
-
-
 # In[ ]:
+big_frame.columns
+
+# %%
+ReturnRate = (big_frame['adjClose']-big_frame['yesterday'])/big_frame['yesterday']
+big_frame.insert(1,"ReturnRate", ReturnRate, True)
 
 
+# %%
+Return = big_frame[[ 'PersianNameOfCode', 'ReturnRate', 'date']].copy()
+# %%
+b = 'Stock'
+c = ['Stock' + str(i) for i in range(len(np.unique(Return['PersianNameOfCode'])))]
+data = {
+  "PersianNameOfCode": np.unique(Return['PersianNameOfCode']),
+  "CodeOfPerasianName": c
+}
+df1 = pd.DataFrame(data)
+Return = pd.merge(Return, df1, on = 'PersianNameOfCode', how = 'left')
+# %%
+G = Return.groupby(['CodeOfPerasianName',  'date'])
+Return1 = G.mean().reset_index()
 
-
+# %%
+#prepare data for regression using pivot
+DataForReqression = Return1.pivot(index='date', columns='CodeOfPerasianName', values='ReturnRate').fillna(0)
+#DataForReqression0 = pd.merge(DataForReqression,Index, on = 'date', how = 'left')
+# %%
+DataForReqression.to_csv("/home/khaled/Project/Data/LossCalculate/ReturnModified.csv")
+# %%
