39f8e269337370ac784c042f66a97b03426a3b5f
Xaleed <masoumifardk@yahoo.com>
2023-03-13 15:47:03 +0330

    this data is not good and full of zero number. one way for solve this problem is remove zero from data, but at firt we should filter data that correlate with dependence variable

--- DataCleaning.py
+++ DataCleaning.py
@@ -13,15 +13,13 @@ from pytse_client import download_client_types_records
 
 
 # In[2]:
-
-
 #Download historical data of trades
 #tickers = tse.download(symbols="all", write_to_csv=True,  include_jdate=True)
 
 
 # In[3]:
-#path = r'C:\\Users\\masoumifard.kh\\CodsOfprojects\\Public\\PredictionOfPumpAndDump\\tickers_data'
-path = r"/home/khaled/Project/DataScienceInStockForecasting/tickers_data/"
+path = r'C:/Users/masoumifard.kh/CodsOfprojects/Public/DataScienceInStockForecasting/tickers_data/'
+#path = r"/home/khaled/Project/DataScienceInStockForecasting/tickers_data/"
 filenames = glob.glob(path + "/*.csv")
 # In[4]:
 def path_leaf(path):
@@ -35,16 +33,26 @@ for filename in filenames:
     dd.insert(1,"PersianNameOfCode", strs, True)
     dfs.append(dd)
 big_frame = pd.concat(dfs, ignore_index=True)
+#%%
+big_frame1 = big_frame[big_frame['date'] > big_frame['date'][2500]].copy()
 # In[ ]:
-big_frame.columns
+big_frame1 = big_frame1[(big_frame1['yesterday'] != 0) & (big_frame1['value'] > 10)]
+#big_frame.columns
 
 # %%
-ReturnRate = (big_frame['adjClose']-big_frame['yesterday'])/big_frame['yesterday']
-big_frame.insert(1,"ReturnRate", ReturnRate, True)
+def frac(a,d):
+    return(a/d)
+fr = big_frame1.apply(lambda x:frac(x['adjClose'],x['yesterday']), 
+                        axis=1)
 
+#%%
+big_frame1.insert(1,"ReturnRate", fr, True)
+
+#%%
+big_frame1 = big_frame1[(big_frame1['ReturnRate'] > np.quantile(big_frame1['ReturnRate'],0.001)) & (big_frame1['ReturnRate'] < np.quantile(big_frame1['ReturnRate'],0.999))]
 
 # %%
-Return = big_frame[[ 'PersianNameOfCode', 'ReturnRate', 'date']].copy()
+Return = big_frame1[[ 'PersianNameOfCode', 'ReturnRate', 'date']].copy()
 # %%
 b = 'Stock'
 c = ['Stock' + str(i) for i in range(len(np.unique(Return['PersianNameOfCode'])))]
@@ -54,14 +62,24 @@ data = {
 }
 df1 = pd.DataFrame(data)
 Return = pd.merge(Return, df1, on = 'PersianNameOfCode', how = 'left')
+Return =Return[[ 'ReturnRate', 'date', 'CodeOfPerasianName']]
 # %%
 G = Return.groupby(['CodeOfPerasianName',  'date'])
 Return1 = G.mean().reset_index()
 
+#%%
+Return1 = Return1.iloc[:,[0,1,3]]
 # %%
 #prepare data for regression using pivot
 DataForReqression = Return1.pivot(index='date', columns='CodeOfPerasianName', values='ReturnRate').fillna(0)
 #DataForReqression0 = pd.merge(DataForReqression,Index, on = 'date', how = 'left')
 # %%
-DataForReqression.to_csv("/home/khaled/Project/Data/LossCalculate/ReturnModified.csv")
+
+# %%
+DataForReqression.to_csv("C:/Users/masoumifard.kh/CodsOfprojects/Public/DataScienceInStockForecasting/ReturnModified.csv")
+
+# %%
+
+# %%
+Return.columns
 # %%
